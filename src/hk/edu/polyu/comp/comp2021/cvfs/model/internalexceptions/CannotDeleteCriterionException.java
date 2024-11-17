b24b23c90f08864b0ac972a26fc907e2499bd6da
FrankYang0610 <yxk0610@icloud.com>
2024-11-18 00:41:34 +0800

    Fix indentation in `CannotDeleteCriterionException`

--- src/hk/edu/polyu/comp/comp2021/cvfs/model/internalexceptions/CannotDeleteCriterionException.java
+++ src/hk/edu/polyu/comp/comp2021/cvfs/model/internalexceptions/CannotDeleteCriterionException.java
@@ -7,11 +7,11 @@ import hk.edu.polyu.comp.comp2021.cvfs.model.ModelException;
  * Thrown to indicate that the criterion cannot be deleted. Generally, this is because other criteria depend on this criterion.
  */
 public class CannotDeleteCriterionException extends ModelException {
-  /**
-   * Construct a new exception.
-   */
-  public CannotDeleteCriterionException() {
-    super("The criterion cannot be deleted. Possibly because other criterion depend on this criterion.");
-  }
+    /**
+     * Construct a new exception.
+     */
+    public CannotDeleteCriterionException() {
+        super("The criterion cannot be deleted. Possibly because other criterion depend on this criterion.");
+    }
 }
 
